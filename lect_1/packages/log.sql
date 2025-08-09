86a987caaa70db0a049259529185db9e13cb803e
annabogo999 <anna.bogo@gmail.com>
2025-08-09 13:35:50 +0000

    Sat, Aug 9, 2025, 4:35 PM +03:00

--- lect_1/packages/log.sql
+++ lect_1/packages/log.sql
@@ -2,10 +2,7 @@
 -- Searching for the address and type of the Lost Letter
 SELECT "address", "type" FROM "addresses" WHERE "id" = (
      SELECT "address_id" FROM "scans" WHERE "action" = 'Drop' AND "address_id" != (
-        SELECT "to_address_id" FROM "packages" WHERE "to_address_id" = (
-            SELECT "id" FROM "addresses" WHERE "address" LIKE '%2%innegan%treet%'
-        )
-        OR "from_address_id" = (
+        SELECT "to_address_id" FROM "packages" WHERE "from_address_id" = (
             SELECT "id" FROM "addresses" WHERE "address" LIKE '%900%omerville%venue%'
         )
      )
