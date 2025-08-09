1863fe02971ff87b9721fc73e440bcc6f26a67d9
annabogo999 <anna.bogo@gmail.com>
2025-08-09 21:10:01 +0000

    Sun, Aug 10, 2025, 12:10 AM +03:00

--- lect_1/dese/8.sql
+++ lect_1/dese/8.sql
@@ -3,7 +3,7 @@ FROM "districts"
 JOIN "expenditures"
 ON "expenditures"."district_id" = "districts"."id";
 
-SELECT "districts"."name", "expenditures"."pupils",
+SELECT "districts"."name", "districts"."id", "expenditures"."pupils",
 FROM "districts"
 JOIN "expenditures"
 ON "expenditures"."district_id" = "districts"."id";
