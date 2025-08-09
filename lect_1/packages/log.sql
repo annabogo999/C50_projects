e7ac12bec5048f53a79c1c6e038e032f0f19094a
annabogo999 <anna.bogo@gmail.com>
2025-08-09 13:23:23 +0000

    Sat, Aug 9, 2025, 4:23 PM +03:00

--- lect_1/packages/log.sql
+++ lect_1/packages/log.sql
@@ -1,4 +1,5 @@
 -- *** The Lost Letter ***
+-- Searching for the address and type of the Lost Letter
 SELECT "address", "type" FROM "addresses" WHERE "id" = (
      SELECT "address_id" FROM "scans" WHERE "action" = 'Drop' AND "address_id" != (
         SELECT "to_address_id" FROM "packages" WHERE "to_address_id" = (
@@ -10,23 +11,27 @@ SELECT "address", "type" FROM "addresses" WHERE "id" = (
      )
 );
 -- *** The Devious Delivery ***
+-- Searching for the address type of the Devious Delivery
 SELECT "type" FROM "addresses" WHERE "id" = (
      SELECT "address_id" FROM "scans" WHERE "action" = 'Drop' AND "address_id" != (
         SELECT "to_address_id" FROM "packages" WHERE "from_address_id" IS NULL
      )
 );
+-- Searching for the contents of the Devious Delivery
 SELECT "contents" FROM "packages" WHERE "id" = (
     SELECT "package_id" FROM "scans" WHERE "action" = 'Drop' AND "address_id" != (
         SELECT "to_address_id" FROM "packages" WHERE "from_address_id" IS NULL
      )
 );
 -- *** The Forgotten Gift ***
+-- Searching for the contents of the Forgotten Gift
 SELECT "contents" FROM "packages" WHERE "from_address_id" = (
     SELECT "id" FROM "addresses" WHERE "address" LIKE '%109%ileston%'
 )
 AND "to_address_id" = (
      SELECT "id" FROM "addresses" WHERE "address" LIKE '%728%aple%lace'
 );
+-- Searching for the name of the driver that has flowers
 SELECT "name" FROM "drivers" WHERE "id" = (
     SELECT "driver_id" FROM "scans" WHERE "package_id" = (
         SELECT "id" FROM "packages" WHERE "from_address_id" = (
