55e36f49f0fd0bcca5d9aab94fb2d6eafd616a70
annabogo999 <anna.bogo@gmail.com>
2025-08-09 12:19:54 +0000

    Sat, Aug 9, 2025, 3:19 PM +03:00

--- lect_1/packages/log.sql
+++ lect_1/packages/log.sql
@@ -12,12 +12,7 @@ SELECT "address", "type" FROM "addresses" WHERE "id" = (
 -- *** The Devious Delivery ***
 SELECT "address" FROM "addresses" WHERE "id" = (
      SELECT "address_id" FROM "scans" WHERE "action" = 'Drop' AND "address_id" != (
-        SELECT "to_address_id" FROM "packages" WHERE "to_address_id" = (
-            SELECT "id" FROM "addresses" WHERE "address" LIKE '%2%innegan%treet%'
-        )
-        OR "from_address_id" = (
-            SELECT "id" FROM "addresses" WHERE "address" LIKE '%900%omerville%venue%'
-        )
+        SELECT "to_address_id" FROM "packages" WHERE "from_address_id" IS NULL 
      )
 );
 -- *** The Forgotten Gift ***
