e87cfb8a4a2db846198ff66b55b8590a37ca069d
annabogo999 <anna.bogo@gmail.com>
2025-08-09 20:43:37 +0000

    Sat, Aug 9, 2025, 11:43 PM +03:00

--- lect_1/dese/8.sql
+++ lect_1/dese/8.sql
@@ -1,4 +1,5 @@
 SELECT "district_id", "pupils" FROM "expenditures"
-SELECT "name" FROM "districts" WHERE "id" = (
-    SELECT "district_id" FROM "expenditures";
-)
+SELECT "name", "id" FROM "districts" WHERE "id" IN (
+    SELECT "district_id" FROM "expenditures"
+);
+JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id";
