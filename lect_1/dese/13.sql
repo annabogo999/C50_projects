cbbb33978660e08f3c355cb40dcd68b1d7f426d6
annabogo999 <anna.bogo@gmail.com>
2025-08-10 10:05:17 +0000

    Sun, Aug 10, 2025, 1:05 PM +03:00

---
+++ lect_1/dese/13.sql
@@ -0,0 +1,15 @@
+SELECT "districts"."name", "expenditures"."per_pupil_expenditure", "staff_evaluations"."exemplary"
+FROM "districts"
+JOIN "expenditures"
+ON "districts"."id" = "expenditures"."district_id"
+JOIN "staff_evaluations"
+ON "districts"."id" = "staff_evaluations"."district_id"
+WHERE "unsatisfactory" > (
+    SELECT AVG ("exemplary") FROM "staff_evaluations"
+)
+AND
+"per_pupil_expenditure" > (
+    SELECT AVG("per_pupil_expenditure") FROM "expenditures"
+)
+AND "districts"."type" = 'Public School District'
+ORDER BY "exemplary" DESC, "per_pupil_expenditure" DESC;
