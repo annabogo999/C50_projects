d526f8156d8302a9bd243ca8d8fd6ebf00c4a7d6
annabogo999 <anna.bogo@gmail.com>
2025-08-10 09:35:44 +0000

    Sun, Aug 10, 2025, 12:35 PM +03:00

--- lect_1/dese/11.sql
+++ lect_1/dese/11.sql
@@ -1,5 +1,6 @@
 SELECT "schools"."name", "expenditures"."per_pupil_expenditure", "graduation_rates"."graduated"
 FROM "schools"
 JOIN "expenditures"
+ON "schools"."distric
 JOIN "graduation_rates"
 ORDER BY "expenditures"."per_pupil_expenditure" DESC, "schools"."name";
