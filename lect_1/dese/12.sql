0a0f9905cffcc505b7f46b50fecebce81fcb88fb
annabogo999 <anna.bogo@gmail.com>
2025-08-10 09:59:47 +0000

    Sun, Aug 10, 2025, 12:59 PM +03:00

--- lect_1/dese/12.sql
+++ lect_1/dese/12.sql
@@ -1,13 +1,13 @@
 SELECT "districts"."name", "expenditures"."per_pupil_expenditure", "staff_evaluations"."exemplary"
 FROM "districts"
 JOIN "expenditures"
-ON "districts"."id" = "expenditures"."districts_id"
+ON "districts"."id" = "expenditures"."district_id"
 JOIN "staff_evaluations"
-ON "districts"."id" = "staff_evaluations"."districts_id"
+ON "districts"."id" = "staff_evaluations"."district_id"
 WHERE "exemplary" > SELECT (
-    AVG ("exemplary")
+    AVG ("exemplary") FROM "staff_evaluations"
 ) AND
 "per_pupil_expenditure" > SELECT (
-    AVG("per_pupil_expenditure")
+    AVG("per_pupil_expenditure") FROM "expenditures"
 ) AND "districts"."type" = 'Public School District';
 
