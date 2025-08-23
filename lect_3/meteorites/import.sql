b3bf8bfc747e8489c340decc26701ad4e64e3432
annabogo999 <anna.bogo@gmail.com>
2025-08-23 11:37:01 +0000

    Sat, Aug 23, 2025, 2:37 PM +03:00

--- lect_3/meteorites/import.sql
+++ lect_3/meteorites/import.sql
@@ -1,10 +1,11 @@
 -- IMPORT FROM csv to temp
 .import --csv meteorites.csv temp
+
 -- CLEAN "Relict"
 DELETE FROM "temp"
 WHERE "nametype" = 'Relict';
--- CREATE NEW TEMP TABLE WITH ID
 
+-- CREATE NEW TEMP TABLE WITH ID
 CREATE TABLE "meteorites_temp" (
     "id" INTEGER,
     "name" TEXT NOT NULL UNIQUE,
@@ -16,11 +17,7 @@ CREATE TABLE "meteorites_temp" (
     "long" NUMERIC,
     PRIMARY KEY("id")
 );
-.import --csv meteorites.csv temp
-DROP TABLE IF EXISTS "temp;";
-DROP TABLE IF EXISTS "meteorites_temp";
-.import --csv meteorites.csv meteorites_temp
-INSERT INTO "meteorites_temp1" ("name", "class", "mass", "discovery", "year", "lat", "long")
-SELECT "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
-DELETE FROM "meteorites"
-WHERE "nametype" IS NULL;
+
+-- INSERT TO NEW TEMP ONLY NEEDED
+INSERT INTO "meteorites_temp" ("name", "class", "mass", "discovery", "year", "lat", "long")
+SELECT "name", "class", "mass", "discovery", "year", "lat", "long" FROM "temp";
