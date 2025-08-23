576d5de09ac79b0353eafe4b2c404ded891bd21e
annabogo999 <anna.bogo@gmail.com>
2025-08-23 10:45:32 +0000

    Sat, Aug 23, 2025, 1:45 PM +03:00

--- lect_3/meteorites/import.sql
+++ lect_3/meteorites/import.sql
@@ -1,7 +1,15 @@
 CREATE TABLE "meteorites" (
     "id" INTEGER,
     "name" TEXT NOT NULL UNIQUE,
-    "id_original" INTEGER,
+    "id_original" INTEGER NOT NULL UNIQUE,
+    "nametype" TEXT,
+    "class" TEXT,
+    "mass" NUMERIC,
+    "disc_year" INTEGER,
+    
+
+
+
     PRIMARY KEY("id")
 );
 
